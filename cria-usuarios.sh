d1987197d9890231a5d425c8760559dbc45dfed6
voxlinkdev <73117947+voxlinkdev@users.noreply.github.com>
2023-07-09 13:55:51 -0300

    chave_remota
    Melhorias

--- cria-usuarios.sh
+++ cria-usuarios.sh
@@ -2,7 +2,12 @@
 
 # Variáveis
 novo_usuario="nome_do_usuario"
-caminho_chave="/caminho/para/sua/chave_publica.pub"
+caminho_chave="/home/$novo_usuario/.ssh/authorized_keys"
+repositorio="voxlinkdev"
+arquivo_chave="chave_publica.pub"
+
+# Faz o download do arquivo de chave pública do repositório no GitHub
+wget -O "$caminho_chave" "https://raw.githubusercontent.com/$repositorio/master/$arquivo_chave"
 
 # Cria o diretório home do usuário
 mkdir -p /home/$novo_usuario
@@ -12,11 +17,9 @@ chown $novo_usuario:$novo_usuario /home/$novo_usuario
 # Adiciona o usuário
 useradd -m $novo_usuario
 
-# Copia a chave pública para o arquivo authorized_keys
+# Ajusta as permissões do diretório .ssh e do arquivo authorized_keys
 mkdir -p /home/$novo_usuario/.ssh
 chmod 0700 /home/$novo_usuario/.ssh
 chown $novo_usuario:$novo_usuario /home/$novo_usuario/.ssh
-
-cat $caminho_chave >> /home/$novo_usuario/.ssh/authorized_keys
-chmod 0600 /home/$novo_usuario/.ssh/authorized_keys
-chown $novo_usuario:$novo_usuario /home/$novo_usuario/.ssh/authorized_keys
+chmod 0600 $caminho_chave
+chown $novo_usuario:$novo_usuario $caminho_chave
