1fcf8ee642b9c8deb4235f76653e683ac03217b6
voxlinkdev <73117947+voxlinkdev@users.noreply.github.com>
2023-07-09 14:35:13 -0300

    fix3
    fix3

--- cria-usuarios.sh
+++ cria-usuarios.sh
@@ -10,22 +10,19 @@ arquivo_chave="pub-spider.txt"
 # Adiciona o usuário
 useradd -m $novo_usuario
 
-# Faz o download do arquivo de chave pública do repositório no GitHub
-wget -O "$caminho_chave" "https://raw.githubusercontent.com/$repositorio/$repositoriov/main/chaves/$arquivo_chave"
+
 
 
 
 
 # Cria o diretório home do usuário
-mkdir -p /home/$novo_usuario
-chmod 0755 /home/$novo_usuario
-chown $novo_usuario:$novo_usuario /home/$novo_usuario
-
-
 
 # Ajusta as permissões do diretório .ssh e do arquivo authorized_keys
 mkdir -p /home/$novo_usuario/.ssh
 chmod 0700 /home/$novo_usuario/.ssh
 chown $novo_usuario:$novo_usuario /home/$novo_usuario/.ssh
+
+# Faz o download do arquivo de chave pública do repositório no GitHub
+wget -O "$caminho_chave" "https://raw.githubusercontent.com/$repositorio/$repositoriov/main/chaves/$arquivo_chave"
 chmod 0600 $caminho_chave
 chown $novo_usuario:$novo_usuario $caminho_chave
